36d3648673e22e7487e04ab252b74d6f5d01fb0e
szebestik@gmail.com <szebestik@gmail.com>
2021-07-31 16:14:48 +0200

    added a scoreboard, changed the game info text, polished look and code

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -6,6 +6,7 @@ const Game = () => {
     const [restartGame, setRestartGame] = useState(false)
     const [playerMove, setPlayerMove] = useState(1)
     const [gameState, setGameState] = useState("")
+    const [score, setScore] = useState({x: 0, o: 0})
 
     const changeGameState = () => {
         setRestartGame(false)
@@ -13,10 +14,24 @@ const Game = () => {
     }
 
     const finishedGame = (message) => {
+        if (message.includes("1"))
+            setScore(prev => {
+                return {
+                    ...prev,
+                    x: prev.x + 1
+                }
+            })
+        else if (message.includes("2")) 
+            setScore(prev => {
+                return {
+                    ...prev,
+                    o: prev.o + 1
+                }
+            })
         setGameState(message)
     }
 
-    const restart = () => {
+    const newGame = () => {
         setPlayerMove(1)
         setGameState("")
         setRestartGame(true)
@@ -24,15 +39,21 @@ const Game = () => {
 
     return (
         <div className={classes.gameContainer}>
+            <div className={classes.scoreContainer}>
+                <p>x: {score.x}</p>
+                <p>o: {score.o}</p>
+            </div>
             <Board
                 restartGame={restartGame}
                 canPlace={gameState === ""}
                 playerMove={playerMove} 
                 changeGameStateFunction={changeGameState}
                 finishedGame={finishedGame}/>
-            <h1>{gameState === "" ? (playerMove === 1 ? "Players 1 move" : "Players 2 move") : gameState}</h1>
-            <div>
-                {gameState !== "" && <button className={`${classes.button} ${classes.buttonPrimary}`} onClick={restart}>Restart?</button>}
+            <h1 className={classes.infoText}>{gameState === "" ? (playerMove === 1 ? "Players 1 move" : "Players 2 move") : gameState}</h1>
+            <div className={classes.buttonContainer}>
+                {gameState !== "" && <button className={`${classes.button} ${classes.buttonPrimary}`} onClick={newGame}>New game</button>}
+                {/*add functionality to changing mode later, when there will be more game modes*/}
+                {/*gameState !== "" && <button className={`${classes.button} ${classes.buttonPrimary}`}>Change game mode</button>*/}
             </div>
         </div>
     )
